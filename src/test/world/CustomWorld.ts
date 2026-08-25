e44b8cb0dae565a21835ee93dc7b570fa19edb1f
Subathra-subu <subathrasubu2004@gmail.com>
2026-08-25 20:45:34 +0530

    Training Program feature added

--- src/test/world/CustomWorld.ts
+++ src/test/world/CustomWorld.ts
@@ -7,7 +7,7 @@ import { SignINPage } from '../pages/SignINPage';
 import { RegisterPage } from '../pages/RegisterPage';
 import { part_prof_mgmt_page } from '../pages/part_prof_mgmt_page';
 import { AdminPage } from '../pages/AdminPage';
-
+import { TrainingProgramPage } from '../pages/TrainingProgramPage';
 
 export class CustomWorld extends World{
     browser!:Browser;
@@ -21,6 +21,7 @@ export class CustomWorld extends World{
     ppm!:part_prof_mgmt_page;
     skillCountBeforeAction!: number;
     ap!:AdminPage;
+    tp!:TrainingProgramPage;
 }
 
 setWorldConstructor(CustomWorld);
